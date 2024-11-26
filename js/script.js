e9e7a328c97db2bad56ed091f636845e097981b7
Pol <pol.moreno.garcia@outlook.com>
2024-11-26 20:15:33 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,104 @@
+(function ($) {
+
+  "use strict";
+
+  // background color when scroll 
+  var initScrollNav = function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 200) {
+      $('.navbar.fixed-top').addClass("bg-primary");
+    } else {
+      $('.navbar.fixed-top').removeClass("bg-primary");
+    }
+  }
+
+  $(window).scroll(function () {
+    initScrollNav();
+  });
+
+
+  // document ready
+  $(document).ready(function () {
+
+    var swiper = new Swiper(".testimonial-swiper", {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+
+
+    // product single page
+    var thumb_slider = new Swiper(".product-thumbnail-slider", {
+      loop: true,
+      slidesPerView: 3,
+      autoplay: true,
+      direction: "vertical",
+      spaceBetween: 30,
+    });
+
+    var large_slider = new Swiper(".product-large-slider", {
+      loop: true,
+      slidesPerView: 1,
+      autoplay: true,
+      effect: 'fade',
+      thumbs: {
+        swiper: thumb_slider,
+      },
+    });
+
+    window.addEventListener("load", (event) => {
+      //isotope
+      $('.isotope-container').isotope({
+        // options
+        itemSelector: '.item',
+        layoutMode: 'masonry',
+      });
+
+
+
+      // Initialize Isotope
+      var $container = $('.isotope-container').isotope({
+        // options
+        itemSelector: '.item',
+        layoutMode: 'masonry',
+      });
+
+      $(document).ready(function () {
+        //active button
+        $('.filter-button').click(function () {
+          $('.filter-button').removeClass('active');
+          $(this).addClass('active');
+        });
+      });
+
+      // Filter items on button click
+      $('.filter-button').click(function () {
+        var filterValue = $(this).attr('data-filter');
+        if (filterValue === '*') {
+          // Show all items
+          $container.isotope({ filter: '*' });
+        } else {
+          // Show filtered items
+          $container.isotope({ filter: filterValue });
+        }
+      });
+
+    });
+
+    $(".youtube").colorbox({
+      iframe: true,
+      innerWidth: 960,
+      innerHeight: 585
+    });
+
+
+    initChocolat();
+
+  }); // End of a document
+
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# 🚀 My Personal Portfolio
+Welcome to my personal portfolio! This project showcases my skills, experience, and passion as a Mechanical Engineer and Software Developer. It has been carefully designed and developed to reflect my journey and projects in both engineering and programming.
+
+## **Project Overview**
+This portfolio is based on a template designed by Rickard Max from TemplatesJungle. I've extensively customized and modified it to better fit my personal style and needs. The website is designed to be fully responsive and optimized to give visitors a seamless experience across all devices.
+
+### **Technologies Used**
+- **HTML5** - For structure
+- **CSS3** - For styling
+- **JavaScript** - For interactivity
+- **Swiper** - For the interactive project carousel
+- **Font Awesome & Iconify** - For icons and visual enhancements
+-  **Bootstrap** - For responsive grid and utilities
